3bb32ef95bbd3aa777f72731f1c8e9224f72419f
xavitje <111566668+xavitje@users.noreply.github.com>
2025-12-04 15:47:24 +0100

    Update check-user.js

--- backend/check-user.js
+++ backend/check-user.js
@@ -1,13 +1,12 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
 const User = require('./models/User');
 
-const MONGODB_URI = 'mongodb+srv://rafielidrissi_db_user:Rafi2506@widgetcluster.918yozi.mongodb.net/?appName=WidgetCluster';
+const URI = 'mongodb+srv://rafielidrissi_db_user:Rafi2506@widgetcluster.918yozi.mongodb.net/?appName=WidgetCluster';
 
 (async () => {
-  await mongoose.connect(MONGODB_URI);
-  const user = await User.findOne({ email: 'test@site.nl' }); // pas aan naar bestaand e-mailadres
+  await mongoose.connect(URI);
+  const user = await User.findOne({ email: 'test@site.nl' }); // pas aan indien nodig
   console.log('Document in DB:', JSON.stringify(user, null, 2));
-  console.log('passwordHash veld:', user.passwordHash);
+  console.log('passwordHash veld:', user?.passwordHash);
   await mongoose.disconnect();
 })();
